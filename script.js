ac25ba7176dc07108140f0f631f9b02c747e2e5d
Deepthi Suvarna <deepthi80089@gmail.com>
2025-06-03 16:07:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showFact(painting) {
+  const facts = {
+    mona: "The Mona Lisa has no clearly visible eyebrows. This was fashionable in the Renaissance!",
+    starry: "Van Gogh painted Starry Night while in a mental asylum in Saint-Rémy, France.",
+    scream: "‘The Scream’ was inspired by a panic attack Edvard Munch had while walking at sunset.",
+    persistence: "Dalí’s melting clocks represent the fluidity of time in the dream world.",
+    girl: "‘Girl with a Pearl Earring’ is often called the 'Mona Lisa of the North'."
+  };
+
+  alert(facts[painting]);
+}
